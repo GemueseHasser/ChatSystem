62ab96b7a8dcf12b61ead66d4f47c2fd1b068c15
Jonas Lobe <jonas.lobe.2006@outlook.com>
2022-07-29 17:10:30 +0200

    implement chat blacklist

--- src/main/java/de/gemuesehasser/ChatSystem.java
+++ src/main/java/de/gemuesehasser/ChatSystem.java
@@ -1,6 +1,9 @@
 package de.gemuesehasser;
 
+import de.gemuesehasser.listener.ChatListener;
 import de.gemuesehasser.task.NewsTask;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ChatSystem extends JavaPlugin {
@@ -19,13 +22,17 @@ public final class ChatSystem extends JavaPlugin {
         getConfig().options().copyDefaults(true);
         saveConfig();
 
-        // initialise news task
+        // initialize news task
         new NewsTask().runTaskTimerAsynchronously(
             this,
             500,
             getConfig().getInt("newsPeriodMinutes") * 1200L
         );
         getLogger().info("Der News-Task wurde erfolgreich geladen.");
+
+        // initialize listener
+        final PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new ChatListener(), this);
 
         getLogger().info("Das Plugin wurde erfolgreich aktiviert!");
     }
---
+++ src/main/java/de/gemuesehasser/listener/ChatListener.java
@@ -0,0 +1,29 @@
+package de.gemuesehasser.listener;
+
+import de.gemuesehasser.ChatSystem;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public final class ChatListener implements Listener {
+
+    @EventHandler
+    public void onChat(@NotNull final AsyncPlayerChatEvent e) {
+        String message = e.getMessage();
+
+        final List<String> blacklist = ChatSystem.getInstance().getConfig().getStringList("blacklist");
+        final List<String> replace = ChatSystem.getInstance().getConfig().getStringList("replace");
+
+        for (int i = 0; i < blacklist.size(); i++) {
+            final String replaceWord = i >= replace.size() ? replace.get(replace.size() - 1) : replace.get(i);
+
+            message = message.replaceAll("(?i)" + blacklist.get(i), replaceWord);
+        }
+
+        e.setMessage(message);
+    }
+
+}
